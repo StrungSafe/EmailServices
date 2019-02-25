9a1fe4ea230a1e59323b04638372ea9a9dbb0fb3
Miguel Molina Jr <molim134@gmail.com>
2019-02-24 22:12:09 -0500

    Initial check-in

---
+++ EmailServices/EmailServices.Core/IEmailCredentials.cs
@@ -0,0 +1,15 @@
+﻿namespace EmailServices.Core
+{
+    public interface IEmailCredentials
+    {
+        string Address { get; }
+
+        string DisplayName { get; }
+
+        string HostName { get; }
+
+        string Password { get; }
+
+        int Port { get; }
+    }
+}
---
+++ EmailServices/EmailServices.Core/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace EmailServices.Core
+{
+    public interface IEmailService
+    {
+        EmailResult SendEmail(string name, string email, string subject, string body);
+    }
+}
